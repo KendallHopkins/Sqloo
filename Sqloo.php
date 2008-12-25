f34db21bd0629d4b27704ba340c572b4e00354ca
Kendall Hopkins <softwareelves@gmail.com>
2008-12-25 20:55:45 +0000

    Beginning to phpdoc Sqloo.

--- Sqloo.php
+++ Sqloo.php
@@ -76,6 +76,14 @@ class Sqloo
 	private $_load_all_tables_function;
 	private $_selected_master_db_name = NULL;
 	
+	/**
+	*	Construct Function
+	*	@param string Function that is called when Sqloo needs a master db configuration, should return a random master configuration
+	*	@param string Function that is called when Sqloo needs a slave db configuration, should return a random slave configuration
+	*	@param string Function that is called when Sqloo needs to access a table that isn't loaded, allows dynamically loaded tables
+	*	@param string Function that is called when Sqloo needs to access all tables, allows dynamically loaded tables
+	*/
+	
 	public function __construct( $master_db_function, $slave_db_function = NULL, $load_table_function = NULL, $load_all_tables_function = NULL ) 
 	{
 		$this->_master_db_function = $master_db_function;
@@ -83,6 +91,10 @@ class Sqloo
 		$this->_load_table_function = $load_table_function;
 		$this->_load_all_tables_function = $load_all_tables_function;
 	}
+	
+	/**
+	*	Destruct Function
+	*/
 	
 	public function __destruct()
 	{
@@ -92,13 +104,23 @@ class Sqloo
 		}
 	}
 	
-	public function __clone() { trigger_error( "Clone is not allowed.", E_USER_ERROR ); }
-		
+	private function __clone() { trigger_error( "Clone is not allowed.", E_USER_ERROR ); }
+	
+	/**
+	*	Opens a new transaction layer
+	*
+	*	This can be nested to allow multiable layers of transactions
+	*/
+	
 	public function beginTransaction()
 	{
 		$this->_in_transaction++;
 		$this->query( "BEGIN" );
 	}
+	
+	/**
+	*	Rollbacks the outer transaction layer
+	*/
 	
 	public function rollbackTransaction()
 	{
@@ -107,6 +129,10 @@ class Sqloo
 		$this->_in_transaction--;
 	}
 	
+	/**
+	*	Commits the outer transaction layer
+	*/
+	
 	public function commitTransaction()
 	{
 		if( $this->_in_transaction === 0 ) trigger_error( "not in a transaction, didn't commit", E_USER_ERROR );
@@ -114,11 +140,28 @@ class Sqloo
 		$this->_in_transaction--;
 	}
 	
+	/**
+	*	Get a new Sqloo_Query object
+	*
+	*	@returns Sqloo_Query
+	*/
+	
 	public function newQuery()
 	{
 		require_once( "Sqloo/Query.php" );
 		return new Sqloo_Query( $this );
 	}
+	
+	/**
+	*	Insert a new row into a table
+	*
+	*	This function will set the columns "added" and "modified" to the current date if they exist in the table
+	*
+	*	@param	string	Name of the table
+	*	@param	array	Array with the attributes to be inserted, IE array( "column_name1" => "value1", "column_name2" => "value2" )
+	*	@param	string	Insert modifier: insert_low_priority, insert_high_priority or insert_delayed
+	*	@return	int		The id of the inserted row
+	*/
 	
 	public function insert( $table_name, $insert_array, $modifier = NULL )
 	{		
@@ -134,6 +177,16 @@ class Sqloo
 		$this->query( $insert_string );
 		return mysql_insert_id( $this->_getMasterResource() );
 	}
+	
+	/**
+	*	Update a list of rows with new values
+	*
+	*	This function will set the column "modified" to the current date if they exist in the table
+	*
+	*	@param	string	Name of the table
+	*	@param	array	Array with the attributes to be modified, IE array( "column_name1" => "value1", "column_name2" => "value2" )
+	*	@param	array	Array of positive int values that are the id's for the rows you want to update
+	*/
 	
 	public function update( $table_name, $update_array, $id_array )
 	{
@@ -151,6 +204,13 @@ class Sqloo
 		$this->query( $update_string );
 	}
 	
+	/**
+	*	Delete a list of rows
+	*
+	*	@param	string	Name of the table
+	*	@param	array	Array of positive int values that are the id's for the rows you want to delete
+	*/
+	
 	public function delete( $table_name, $id_array )
 	{
 		$id_array_count = count( $id_array );
@@ -162,6 +222,15 @@ class Sqloo
 		$this->query( $delete_string );
 	}
 	
+	/**
+	*	Make a union from multiable Sqloo_Query objects
+	*
+	*	All Sqloo_Query objects must have the same output column names for this to work
+	*
+	*	@param	array		Array of Sqloo_Query objects
+	*	@return	Sqloo_Query	Sqloo_Query object preloaded with a union of the Sqloo_Query objects in $array_of_queries
+	*/
+	
 	public function union( $array_of_queries )
 	{
 		if( count( $array_of_queries ) < 2 ) trigger_error( "union must have more than 1 query objects", E_USER_ERROR );
@@ -169,7 +238,12 @@ class Sqloo
 		return new Sqloo_Query( $this, $array_of_queries );
 	}
 	
-	/* Schema Setup */
+	/**
+	*	Make a new Sqloo_Table Object and return it.
+	*	
+	*	@param	string		New table name
+	*	@return	Sqloo_Table	Empty Sqloo_Table object
+	*/
 	
 	public function newTable( $table_name )
 	{
@@ -177,18 +251,26 @@ class Sqloo
 		return $this->_table_array[ $table_name ] = new Sqloo_Table( $table_name );
 	}
 	
-	public function newNMTable( $table1, $table2 )
+	/**
+	*	Make a new Sqloo_Table Object, setup the parents and return it.
+	*	
+	*	@param	Sqloo_Table	Parent table 1
+	*	@param	Sqloo_Table	Parent table 2
+	*	@return	Sqloo_Table	N:M Sqloo_Table object, used for NMJoins
+	*/
+	
+	public function newNMTable( $sqloo_table_class_1, $sqloo_table_class_2 )
 	{
-		$many_to_many_table = $this->newTable( self::computeNMTableName( $table1->name, $table2->name ) );
+		$many_to_many_table = $this->newTable( self::computeNMTableName( $sqloo_table_class_1->name, $sqloo_table_class_2->name ) );
 		$many_to_many_table->parent = array(
-			$table1->name => array(
-				Sqloo::parent_table_name => $table1->name, 
+			$sqloo_table_class_1->name => array(
+				Sqloo::parent_table_name => $sqloo_table_class_1->name, 
 				Sqloo::allow_null => FALSE, 
 				Sqloo::on_delete => Sqloo::action_cascade, 
 				Sqloo::on_update => Sqloo::action_cascade
 			),
-			$table2->name => array(
-				Sqloo::parent_table_name => $table2->name, 
+			$sqloo_table_class_2->name => array(
+				Sqloo::parent_table_name => $sqloo_table_class_2->name, 
 				Sqloo::allow_null => FALSE, 
 				Sqloo::on_delete => Sqloo::action_cascade, 
 				Sqloo::on_update => Sqloo::action_cascade
@@ -197,7 +279,14 @@ class Sqloo
 		return $many_to_many_table;
 	}
 	
-	/* Utilities */
+	/**
+	*	Runs the $query_string on a database determinded by the params
+	*	
+	*	@param	string		Query string
+	*	@param	bool		Set if the query can be run on a slave. If in a transaction, this is ignored.
+	*	@param	bool		Set if the query should be buffered. Unbuffered queries have advantages and disadvantages.
+	*	@return	resource	Resource from mysql_query or mysql_unbuffered_query.
+	*/
 	
 	public function query( $query_string, $on_slave = FALSE, $buffered = TRUE )
 	{
@@ -213,14 +302,12 @@ class Sqloo
 		return $resource;
 	}
 	
-	public function nextId( $tableName )
-	{
-		$query = "SHOW TABLE STATUS WHERE name = '".$tableName."';";
-		$resource = $this->query( $query );
-		$array = @mysql_fetch_assoc( $resource );
-		if( $array == FALSE ) trigger_error( "bad table name", E_USER_ERROR );
-		return $array[ "Auto_increment" ];
-	}
+	/**
+	*	Escapes values for a query
+	*	
+	*	@param	mixed	The value to be escaped, if a class object is passed it will attempt to be transformed into a string.
+	*	@return	string	Processed value
+	*/
 	
 	static public function processVariable( $value )
 	{
@@ -240,9 +327,19 @@ class Sqloo
 		}
 	}
 	
-	static public function computeNMTableName( $first_Table, $second_table )
+	/**
+	*	Used to compute the name of the NM between two tables.
+	*
+	*	Order they are put in makes no difference to the output.
+	*
+	*	@param	string	First table name
+	*	@param	string	Second table name
+	*	@return	string	NM Table name
+	*/
+	
+	static public function computeNMTableName( $table_name_1, $table_name_2 )
 	{
-		return ( $first_Table < $second_table ) ? $first_Table."-".$second_table : $second_table."-".$first_Table;
+		return ( $table_name_1 < $table_name_2 ) ? $table_name_1."-".$table_name_2 : $table_name_2."-".$table_name_1;
 	}
 	
 	static public function arrayToIn( $value_array )
@@ -258,6 +355,12 @@ class Sqloo
 		foreach( $key_value_array as $key => $value ) $string .= $key."=".$this->processVariable( $value ).",";
 		return rtrim( $string, "," );
 	}
+	
+	/**
+	*	Checks and correct the database schema to match the table schema setup in code.
+	*
+	*	@return	string	Log of the queries run.
+	*/
 	
 	public function checkSchema()
 	{
