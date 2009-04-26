9350470e254789e2152dea157fae68d51ef2fda7
Kendall Hopkins <softwareelves@gmail.com>
2009-04-26 16:12:41 +0000

    Update License to 2009

--- Sqloo/Query/Results.php
+++ Sqloo/Query/Results.php
@@ -3,7 +3,7 @@
 /*
 The MIT License
 
-Copyright (c) 2008 Kendall Hopkins
+Copyright (c) 2009 Kendall Hopkins
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
