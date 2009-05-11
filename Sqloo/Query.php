30158787f3917d2683f5cfebeeda1451597a3d60
Kendall Hopkins <softwareelves@gmail.com>
2009-05-11 17:47:31 +0000

    Re-add back get by reference for query properties. If you try to touch these, we are going to think you changed something, so don't touch unless you need to.

--- Sqloo/Query.php
+++ Sqloo/Query.php
@@ -114,11 +114,12 @@ class Sqloo_Query
 	*	@return	mixed	The attribute value
 	*/
 	
-	public function __get( $key )
+	public function & __get( $key )
 	{
 		if( ! array_key_exists( $key, $this->_query_data ) )
 			trigger_error( "Bad key: $key", E_USER_ERROR );
 		
+		$this->_releaseStatementObject();
 		return $this->_query_data[$key];
 	}
 	
