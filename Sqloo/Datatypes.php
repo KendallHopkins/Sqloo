d5e66d9de5f87eb40ab715ef176e69e3f18f1d12
Kendall Hopkins <softwareelves@gmail.com>
2010-04-05 12:29:46 -0400

    Update Copyright

--- Sqloo/Datatypes.php
+++ Sqloo/Datatypes.php
@@ -3,7 +3,7 @@
 /*
 The MIT License
 
-Copyright (c) 2009 Kendall Hopkins
+Copyright (c) 2010 Kendall Hopkins
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
