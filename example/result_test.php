aba14bedeb655c1412d475be84919d377089fac0
Kendall Hopkins <softwareelves@gmail.com>
2010-04-26 01:02:49 -0400

    Add result example

---
+++ example/result_test.php
@@ -0,0 +1,48 @@
+<?php
+	
+/* SETUP */
+	require_once( "./setup.php" );
+
+/* QUERY EXAMPLES */
+	$sqloo_connection->beginTransaction();
+
+	$item_array = array(
+		array( "name" => "LightSaver" ),
+		array( "name" => "MacBookPro" ),
+		array( "name" => "MacBook" ),
+		array( "name" => "iPod" ),
+		array( "name" => "LightSaber" )
+	);
+	foreach( $item_array as &$insert_data ) {
+		 $inserted_id = $sqloo_connection->insert( "item", $insert_data );
+		 $insert_data["id"] = $inserted_id;
+	}
+	
+	$query = $sqloo_connection->newQuery();
+	$item_table_ref = $query->table( "item" );
+	
+	$sub_string_expression = "SUBSTRING( $item_table_ref->name, 1, 3 )";
+	$query
+		->column( "name", $sub_string_expression )
+		->column( "count", "COUNT( * )" )
+		->where( $item_table_ref->name." LIKE ".$query->parameter( $v = "%Mac%" ) )
+		->order( $sub_string_expression, Sqloo_Query::ORDER_ASCENDING )
+		->group( $sub_string_expression )
+		->having( "COUNT(*) > 1" )
+		->limit( 1, 0 )
+		->offset( 0 )
+		->distinct( FALSE )
+		->lock( Sqloo_Query::SELECT_LOCK_NONE )
+		->run();
+	
+	print_r( $query->count() );
+	print_r( $query->fetchArray() );
+	
+	$query->run();
+	foreach( $query as $row ) {
+		print_r( $row );
+	}	
+
+	$sqloo_connection->rollbackTransaction();
+
+?>
