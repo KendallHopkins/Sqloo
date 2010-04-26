6f11a697871b977ab6d5bcc1a73d3b677fc9d25a
Kendall Hopkins <softwareelves@gmail.com>
2010-04-26 01:54:42 -0400

    Clean up the result test

--- example/result_test.php
+++ example/result_test.php
@@ -20,12 +20,12 @@
 	
 	$query = $sqloo_connection->newQuery();
 	$item_table_ref = $query->table( "item" );
-	
 	$sub_string_expression = "SUBSTRING( $item_table_ref->name, 1, 3 )";
+	$search_string = "%Mac%";
 	$query
 		->column( "name", $sub_string_expression )
-		->column( "count", "COUNT( * )" )
-		->where( $item_table_ref->name." LIKE ".$query->parameter( $v = "%Mac%" ) )
+		->column( "count", "COUNT(*)" )
+		->where( $item_table_ref->name." LIKE ".$query->parameter( $search_string ) )
 		->order( $sub_string_expression, Sqloo_Query::ORDER_ASCENDING )
 		->group( $sub_string_expression )
 		->having( "COUNT(*) > 1" )
