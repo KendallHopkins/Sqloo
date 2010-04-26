76e2459baaa55a07e051405a3bf46b8c14f353a6
Kendall Hopkins <softwareelves@gmail.com>
2010-04-25 20:52:26 -0400

    new union test

---
+++ example/union_test.php
@@ -0,0 +1,57 @@
+<?php
+
+/* SETUP */
+	require_once( "setup.php" );
+
+$sqloo_connection->beginTransaction();
+
+	//SETUP THE DATA
+	$people_array = array(
+		array( "name" => "Kendall Hopkins" ),
+		array( "name" => "Steve Jobs" ),
+		array( "name" => "Bill Gates" ),
+		array( "name" => "Linus Torvalds" )
+	);
+	foreach( $people_array as $person_attributes ) {
+		$sqloo_connection->insert( "person", $person_attributes );
+	}
+	
+	$item_array = array(
+		array( "name" => "iPhone" ),
+		array( "name" => "iPad" ),
+		array( "name" => "MacBookPro" ),
+		array( "name" => "Steve Jobs Bobble Head" )
+	);
+	foreach( $item_array as $item_attributes ) {
+		$sqloo_connection->insert( "item", $item_attributes );
+	}
+	
+	//SETUP ITEM QUERY
+	$item_query = $sqloo_connection->newQuery();
+	$item_table_ref = $item_query->table( "item" );
+	$item_query
+		->column( "id", $item_table_ref->id )
+		->column( "name", $item_table_ref->name )
+		->column( "type", "'item'" );
+	
+	//SETUP PERSON QUERY	
+	$person_query = $sqloo_connection->newQuery();
+	$person_table_ref = $person_query->table( "person" );
+	$person_query
+		->column( "id", $person_table_ref->id )
+		->column( "name", $person_table_ref->name )
+		->column( "type", "'people'" );
+		
+	//SETUP UNION QUERY
+	$union_query = $sqloo_connection->union( array( $item_query, $person_query ) );
+	$union_table_ref = $union_query->getTable();
+	$union_query
+		->where( "$union_table_ref->name LIKE 'Steve Jobs%'" )
+		->run();
+	
+	print $union_query;
+	print_r( $union_query->fetchArray() );
+
+$sqloo_connection->rollbackTransaction();
+
+?>
